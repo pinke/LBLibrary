d9f5ac5a8824400556c05d3dd64a3f3096035499
xiepingkang <xiepingkang@apexsoft.com.cn>
2014-08-11 11:39:42 +0800

    none

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 LiveBOS library for cordova.
 usage:
+
+add plugin:
 cordova plugin add git://github.com/pinke/LBLibrary.git
+
+remove plugin:
+cordova plugin remove com.livebos.cordova.library
+
+
+js call:
+window.launch('app://com.livebos.DemoActivity?serviceURL=xxxx','','');
--- www/LiveBOSLibrary.js
+++ www/LiveBOSLibrary.js
@@ -1,58 +1,58 @@
-cordova.define("org.apache.cordova.LiveBOSLibrary.LiveBOSLibrary", function(require, exports, module) {
+cordova.define("com.livebos.cordova.library.LiveBOSLibrary", function (require, exports, module) {
 
-var exec = require('cordova/exec');
-var channel = require('cordova/channel');
-var modulemapper = require('cordova/modulemapper');
-var urlutil = require('cordova/urlutil');
+    var exec = require('cordova/exec');
+    var channel = require('cordova/channel');
+    var modulemapper = require('cordova/modulemapper');
+    var urlutil = require('cordova/urlutil');
 
-function LiveBOSLibrary() {
-   this.channels = {
-        'loadstart': channel.create('loadstart'),
-        'loadstop' : channel.create('loadstop'),
-        'loaderror' : channel.create('loaderror'),
-        'exit' : channel.create('exit')
-   };
-}
-
-LiveBOSLibrary.prototype = {
-    _eventHandler: function (event) {
-        if (event.type in this.channels) {
-            this.channels[event.type].fire(event);
-        }
-    },
-    close: function (eventname) {
-        exec(null, null, "LiveBOSLibrary", "close", []);
-    },
-    addEventListener: function (eventname,f) {
-        if (eventname in this.channels) {
-            this.channels[eventname].subscribe(f);
-        }
-    },
-    removeEventListener: function(eventname, f) {
-        if (eventname in this.channels) {
-            this.channels[eventname].unsubscribe(f);
-        }
-    }
-};
-
-module.exports = function(strUrl, strWindowName, strWindowFeatures) {
-    // Don't catch calls that write to existing frames (e.g. named iframes).
-    if (window.frames && window.frames[strWindowName]) {
-        var origOpenFunc = modulemapper.getOriginalSymbol(window, 'launch');
-        return origOpenFunc.apply(window, arguments);
+    function LiveBOSLibrary() {
+        this.channels = {
+            'loadstart': channel.create('loadstart'),
+            'loadstop': channel.create('loadstop'),
+            'loaderror': channel.create('loaderror'),
+            'exit': channel.create('exit')
+        };
     }
 
-    strUrl = urlutil.makeAbsolute(strUrl);
-    var iab = new LiveBOSLibrary();
-    var cb = function(eventname) {
-       iab._eventHandler(eventname);
+    LiveBOSLibrary.prototype = {
+        _eventHandler: function (event) {
+            if (event.type in this.channels) {
+                this.channels[event.type].fire(event);
+            }
+        },
+        close: function (eventname) {
+            exec(null, null, "LiveBOSLibrary", "close", []);
+        },
+        addEventListener: function (eventname, f) {
+            if (eventname in this.channels) {
+                this.channels[eventname].subscribe(f);
+            }
+        },
+        removeEventListener: function (eventname, f) {
+            if (eventname in this.channels) {
+                this.channels[eventname].unsubscribe(f);
+            }
+        }
     };
 
-    strWindowFeatures = strWindowFeatures || "";
+    module.exports = function (strUrl, strWindowName, strWindowFeatures) {
+        // Don't catch calls that write to existing frames (e.g. named iframes).
+        if (window.frames && window.frames[strWindowName]) {
+            var origOpenFunc = modulemapper.getOriginalSymbol(window, 'launch');
+            return origOpenFunc.apply(window, arguments);
+        }
 
-    exec(cb, cb, "LiveBOSLibrary", "launch", [strUrl, strWindowName, strWindowFeatures]);
-    return iab;
-};
+        strUrl = urlutil.makeAbsolute(strUrl);
+        var iab = new LiveBOSLibrary();
+        var cb = function (eventname) {
+            iab._eventHandler(eventname);
+        };
+
+        strWindowFeatures = strWindowFeatures || "";
+
+        exec(cb, cb, "LiveBOSLibrary", "launch", [strUrl, strWindowName, strWindowFeatures]);
+        return iab;
+    };
 
 
 });
